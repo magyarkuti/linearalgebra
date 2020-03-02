651d70f52d4c02c91694b9ae917c797d815d87ee
Gyula Magyarkuti <magyarkuti@gmail.com>
2020-03-02 10:40:58 +0100

    Clear

--- compile.sh
+++
@@ -1,6 +0,0 @@
-docker run --rm \
-    --name busy_gauss \
-    --user root \
-    --mount type=bind,source=/Users/mgytk/Desktop/linearalgebra,target=/root/target \
-    --workdir=/root/target \
-    mylatex latexmk -lualatex
